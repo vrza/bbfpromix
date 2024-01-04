c6d0c5cf6c20ccce8e17d8ab5d96fbaad87f0e47
Vladimir Vrzić <vvrzic@gmail.com>
2024-01-04 13:34:59 +0100

    Replaced deprecated G_APPLICATION_FLAGS_NONE

--- src/main.c
+++ src/main.c
@@ -266,7 +266,7 @@ int main(int argc, char** argv) {
     app_data.no_signals = false;
     int status;
 
-    app = gtk_application_new ("de.bollie.babymixpro", G_APPLICATION_FLAGS_NONE);
+    app = gtk_application_new ("de.bollie.babymixpro", G_APPLICATION_DEFAULT_FLAGS);
     g_signal_connect (app, "activate", G_CALLBACK (activate), &app_data);
     status = g_application_run (G_APPLICATION (app), argc, argv);
     if (app_data.mixer)
